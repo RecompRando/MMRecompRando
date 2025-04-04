68b1bbca064d5610ba58bb22017ef4539ef12eed
ThatHypedPerson <thathypedperson@gmail.com>
2025-04-04 02:45:05 -0700

    modify shop items to restock w/ vanilla

--- src/shop_item_hooks.c
+++ src/shop_item_hooks.c
@@ -19,6 +19,13 @@ void EnGirlA_Draw(Actor* thisx, PlayState* play);
 void EnGirlA_InitalUpdate(EnGirlA* this, PlayState* play);
 void EnGirlA_Update2(EnGirlA* this, PlayState* play);
 
+s32 EnGirlA_TrySetMaskItemDescription(EnGirlA* this, PlayState* play);
+void EnGirlA_InitItem(PlayState* play, EnGirlA* this);
+void EnGirlA_Bought(PlayState* play, EnGirlA* this);
+void EnGirlA_Restock(PlayState* play, EnGirlA* this);
+void EnGirlA_DoNothing(EnGirlA* this, PlayState* play);
+void EnGirlA_SetupAction(EnGirlA* this, EnGirlAActionFunc action);
+
 extern ShopItemEntry sShopItemEntries[];
 
 // temporarily set to SI_MAX (43) slots to hold the items for every shop
@@ -52,7 +59,7 @@ void EnGirlA_WaitForObject(EnGirlA* this, PlayState* play) {
 RECOMP_PATCH void EnGirlA_Update(Actor* thisx, PlayState* play) {
     EnGirlA* this = THIS;
 
-    if (!shopObjectLoaded[this->actor.params] && rando_shopsanity_enabled()) {
+    if (!shopObjectLoaded[this->actor.params] && rando_shopsanity_enabled() && !rando_location_is_checked(LOCATION_SHOP_ITEM)) {
         EnGirlA_WaitForObject(this, play);
     }
 
@@ -67,7 +74,7 @@ RECOMP_PATCH void EnGirlA_Draw(Actor* thisx, PlayState* play) {
         this->drawFunc(&this->actor, play, 0);
     }
 
-    if (rando_shopsanity_enabled()) {
+    if (rando_shopsanity_enabled() && !rando_location_is_checked(LOCATION_SHOP_ITEM)) {
         s16 getItemId = rando_get_item_id(LOCATION_SHOP_ITEM);
         if (shopObjectLoaded[this->actor.params]) {
             if (shopObjectStatic[this->actor.params]) {
@@ -84,11 +91,47 @@ RECOMP_PATCH void EnGirlA_Draw(Actor* thisx, PlayState* play) {
 void EnGirlA_RandoBought(PlayState* play, EnGirlA* this) {
     this->isOutOfStock = true;
     this->actor.draw = NULL;
-    this->actor.textId = 0x0841;
 }
 
+// @rando restock with vanilla items by reinitializing w/ default info
 void EnGirlA_RandoRestock(PlayState* play, EnGirlA* this) {
-    EnGirlA_RandoBought(play, this);
+    s16 params = this->actor.params;
+    ShopItemEntry* shopItem = &sShopItemEntries[params];
+
+    if (Object_IsLoaded(&play->objectCtx, this->objectSlot)) {
+        this->actor.flags &= ~ACTOR_FLAG_10;
+        this->actor.objectSlot = this->objectSlot;
+        this->actor.textId = shopItem->descriptionTextId;
+        this->choiceTextId = shopItem->choiceTextId;
+
+        // EnGirlA_TrySetMaskItemDescription always returns false
+        if (!EnGirlA_TrySetMaskItemDescription(this, play)) {
+            EnGirlA_InitItem(play, this);
+        }
+
+        this->boughtFunc = EnGirlA_Bought;
+        this->restockFunc = EnGirlA_Restock;
+        this->getItemId = shopItem->getItemId;
+        this->canBuyFunc = shopItem->canBuyFunc;
+        this->buyFunc = shopItem->buyFunc;
+        this->buyFanfareFunc = shopItem->buyFanfareFunc;
+        this->unk1C0 = 0;
+        this->itemParams = shopItem->params;
+        this->drawFunc = shopItem->drawFunc;
+        this->getItemDrawId = shopItem->getItemDrawId;
+        this->actor.flags &= ~ACTOR_FLAG_TARGETABLE;
+        Actor_SetScale(&this->actor, 0.25f);
+        this->actor.shape.yOffset = 24.0f;
+        this->actor.shape.shadowScale = 4.0f;
+        this->actor.floorHeight = this->actor.home.pos.y;
+        this->actor.gravity = 0.0f;
+        EnGirlA_SetupAction(this, EnGirlA_DoNothing);
+        this->isInitialized = true;
+        this->mainActionFunc = EnGirlA_Update2;
+        this->isSelected = false;
+        this->rotY = 0;
+        this->initialRotY = this->actor.shape.rot.y;
+    }
 }
 
 s32 EnGirlA_RandoCanBuyFunc(PlayState* play, EnGirlA* this) {
@@ -109,7 +152,7 @@ void EnGirlA_RandoBuyFanfare(PlayState* play, EnGirlA* this) {
 }
 
 RECOMP_PATCH void EnGirlA_InitItem(PlayState* play, EnGirlA* this) {
-    if (!rando_shopsanity_enabled()) {
+    if (!rando_shopsanity_enabled() || rando_location_is_checked(LOCATION_SHOP_ITEM)) {
         ShopItemEntry* shopItem = &sShopItemEntries[this->actor.params];
 
         this->actor.textId = shopItem->descriptionTextId;
@@ -122,30 +165,14 @@ RECOMP_PATCH void EnGirlA_InitItem(PlayState* play, EnGirlA* this) {
     shopObjectLoading[this->actor.params] = false;
     shopObjectLoaded[this->actor.params] = false;
 
-    // ShopItemEntry* shopItem = &sShopItemEntries[this->actor.params];
-
-    // this->actor.textId = shopItem->descriptionTextId;
     this->actor.textId = 0x083F;
     this->isOutOfStock = false;
     this->actor.draw = EnGirlA_Draw;
-    if (rando_location_is_checked(LOCATION_SHOP_ITEM)) {
-        this->actor.textId = 0x0841;
-        this->isOutOfStock = true;
-        this->actor.draw = NULL;
-    }
 }
-
-void EnGirlA_DoNothing(EnGirlA* this, PlayState* play);
-void EnGirlA_Update2(EnGirlA* this, PlayState* play);
-s32 EnGirlA_TrySetMaskItemDescription(EnGirlA* this, PlayState* play);
-void EnGirlA_SetupAction(EnGirlA* this, EnGirlAActionFunc action);
-
-void EnGirlA_Bought(PlayState* play, EnGirlA* this);
-void EnGirlA_Restock(PlayState* play, EnGirlA* this);
 
 RECOMP_PATCH void EnGirlA_InitalUpdate(EnGirlA* this, PlayState* play) {
     s16 params = this->actor.params;
-    if (rando_shopsanity_enabled()
+    if (rando_shopsanity_enabled() && !rando_location_is_checked(LOCATION_SHOP_ITEM)
         && !(this->actor.params == SI_BOTTLE ||
             this->actor.params == SI_SWORD_GREAT_FAIRY ||
             this->actor.params == SI_SWORD_KOKIRI ||
@@ -154,7 +181,7 @@ RECOMP_PATCH void EnGirlA_InitalUpdate(EnGirlA* this, PlayState* play) {
             )) {
         s16 trueGI = rando_get_item_id(LOCATION_SHOP_ITEM);
         ShopItemEntry item = { getObjectId(trueGI), getGid(trueGI), NULL, 1, 0x083F, 0x0840, trueGI, EnGirlA_RandoCanBuyFunc,
-        EnGirlA_RandoBuyFunc, EnGirlA_RandoBuyFanfare };
+            EnGirlA_RandoBuyFunc, EnGirlA_RandoBuyFanfare };
         ShopItemEntry* shopItem = &item;
 
         if (Object_IsLoaded(&play->objectCtx, this->objectSlot)) {
@@ -236,4 +263,18 @@ RECOMP_PATCH void EnGirlA_InitalUpdate(EnGirlA* this, PlayState* play) {
             this->initialRotY = this->actor.shape.rot.y;
         }
     }
+}
+
+// @rando prevent vanilla shield from being bought without already having a progressive shield
+RECOMP_PATCH s32 EnGirlA_CanBuyShieldHero(PlayState* play, EnGirlA* this) {
+    if (!rando_has_item(GI_SHIELD_HERO)) {
+        return CANBUY_RESULT_CANNOT_GET_NOW;
+    }
+    if (GET_CUR_EQUIP_VALUE(EQUIP_TYPE_SHIELD) != EQUIP_VALUE_SHIELD_NONE) {
+        return CANBUY_RESULT_NO_ROOM;
+    }
+    if (gSaveContext.save.saveInfo.playerData.rupees < play->msgCtx.unk1206C) {
+        return CANBUY_RESULT_NEED_RUPEES;
+    }
+    return CANBUY_RESULT_SUCCESS_1;
 }
