4d2761ce3fe4c954abc14bd1300b4add856f86eb
Hyped <thathypedperson@gmail.com>
2025-11-28 04:25:10 -0800

    fix more deku flower soul related crashes + move stuff around

--- src/deku_flower_hooks.c
+++ src/deku_flower_hooks.c
@@ -176,3 +176,72 @@ s32 ObjEtcetera_OverrideLimbDraw(PlayState* play, s32 limbIndex, Gfx** dList, Ve
     
     return false;
 }
+
+// fixes for deku flower soul
+
+// deku playground lifts
+#include "overlays/actors/ovl_Obj_Lupygamelift/z_obj_lupygamelift.h"
+
+void func_80AF0394(ObjLupygamelift* this);
+
+RECOMP_PATCH void func_80AF0530(ObjLupygamelift* this, PlayState* play) {
+    f32 distRemaining;
+    Vec3f target;
+
+    target.x = this->points[this->pointIndex].x;
+    target.y = this->points[this->pointIndex].y;
+    target.z = this->points[this->pointIndex].z;
+    distRemaining = Math_Vec3f_StepTo(&this->dyna.actor.world.pos, &target, this->dyna.actor.speed);
+    if (distRemaining > 30.0f) {
+        Math_SmoothStepToF(&this->dyna.actor.speed, this->targetSpeedXZ, 0.5f, 5.0f, 0.1f);
+    } else if (distRemaining > 0.0f) {
+        Math_SmoothStepToF(&this->dyna.actor.speed, 5.0f, 0.5f, 5.0f, 1.0f);
+    } else {
+        if (this->pointIndex < (this->count - 1)) {
+            this->pointIndex++;
+        } else {
+            this->pointIndex = 0;
+        }
+    }
+    // @rando fix bug where the game crashes when there is no deku flower child (soul)
+    if (this->dyna.actor.child != NULL) {
+        if (this->dyna.actor.child->update == NULL) {
+            this->dyna.actor.child = NULL;
+        } else {
+            this->dyna.actor.child->world.pos.x = this->dyna.actor.world.pos.x;
+            this->dyna.actor.child->world.pos.y = this->dyna.actor.world.pos.y;
+            this->dyna.actor.child->world.pos.z = this->dyna.actor.world.pos.z;
+        }
+    }
+    func_80AF0394(this);
+}
+
+// deku trial spinning platforms
+#include "overlays/actors/ovl_Obj_Rotlift/z_obj_rotlift.h"
+
+RECOMP_PATCH void ObjRotlift_MoveDekuFlowers(ObjRotlift* this) {    
+    ObjEtcetera** dekuFlower = this->dekuFlowers;
+    ObjEtcetera* curDekuFlower;
+    f32 posOffset = 300.0f;
+    s32 i;
+
+    for (i = 0; i < ARRAY_COUNT(this->dekuFlowers); i++, dekuFlower++) {
+        curDekuFlower = *dekuFlower;
+
+        // @rando fix bug where the game crashes when there is no deku flower soul
+        if (curDekuFlower != NULL) {
+            if (curDekuFlower->dyna.actor.update == NULL) {
+                *dekuFlower = NULL;
+            } else {
+                curDekuFlower->dyna.actor.world.pos.x =
+                    this->dyna.actor.world.pos.x + posOffset * Math_SinS(this->dyna.actor.shape.rot.y);
+                curDekuFlower->dyna.actor.world.pos.y = this->dyna.actor.world.pos.y + 380.0f * this->dyna.actor.scale.y;
+                curDekuFlower->dyna.actor.world.pos.z =
+                    this->dyna.actor.world.pos.z + posOffset * Math_CosS(this->dyna.actor.shape.rot.y);
+                curDekuFlower->dyna.actor.shape.rot.y = this->dyna.actor.shape.rot.y;
+            }
+        }
+
+        posOffset -= 600.0f;
+    }
+}
