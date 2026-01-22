5d928411d094d6af2531e21d2689f7c4398960cc
G4M3R L1F3 <patrick-doiron@hotmail.com>
2026-01-21 22:47:51 -0400

    Changed text box type to the one most NPCs use

--- src/item_text.c
+++ src/item_text.c
@@ -388,6 +388,10 @@ RECOMP_PATCH void Message_OpenText(PlayState* play, u16 textId) {
         font->msgBuf.schar[0] = 0x06;
         font->msgBuf.schar[1] = 0x71;
     }
+    if (textId == 0x910 || textId == 0x914 || textId == 0x91B || textId == 0x918) {
+        font->msgBuf.schar[0] = 0x00;
+    }
+    
     if ((textId & 0xFF00) == 0x3600 || (textId & 0xFF00) == 0x3700 || (textId == 0x0880 && rando_shopsanity_enabled() && !rando_location_is_checked_async(0x090002))) {
         msg = shop_msg;
         font->msgBuf.schar[0] = 0x06;
