84aa8d662bc3b54bacee7c863395a85037d6ab85
Hyped <thathypedperson@gmail.com>
2026-03-18 04:20:04 -0700

    optimize the glue a ton by moving to the main interpreter + utilizing the caching

--- src/py_init.c
+++ src/py_init.c
@@ -1,6 +1,6 @@
 #include "rando_glue.h"
 
-REPY_REGISTER_SUBINTERPRETER(rando_interp);
+REPY_InterpreterIndex rando_interp = 0;
 
 REPY_ON_POST_INIT void on_repy_post_init() {
     RandoGlue_Init("mm_recomp_rando", "Majora's Mask Recompiled");
