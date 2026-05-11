40552ba1c9cba48d3f0e5ed2a010873bcfe36026
LT-Schmiddy <schmidalex1996@gmail.com>
2026-05-11 17:01:35 -0400

    updating to latest Python glue. Builds

--- include/repy_api.h
+++ include/repy_api.h
@@ -393,7 +393,7 @@ REPY_ON_PRE_INIT void _repy_register_nrm () { \
 #define REPY_ADD_NRM_TO_MAIN_INTERPRETER \
 REPY_ON_CONFIG_INTERPRETERS void __repy_config_main_interpreter() { \
     recomp_printf("Configuring Main Interpreter (Index %i)\n", 0); \
-    REPY_PushInterpreter(subinterp_identifier); \
+    REPY_PushInterpreter(0); \
     REPY_AddNrmToSysPath(); \
     REPY_PopInterpreter(); \
 } 
