e4eacb5166dbc829648cd3b355fffc3138876cf0
Hyped <thathypedperson@gmail.com>
2026-06-18 19:05:16 -0700

    add apworld packaging task modbuild.py

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     print(f"Wrong file! This is the pyinvoke tasks file for the `modbuild.py` tool. Run `python[3] ./modbuild.py` to use it.")
     sys.exit(0)
 
-import shutil, os, json
+import shutil, os, json, zipfile
 from pathlib import Path
 
 from modbuildcore.jobs import *
@@ -330,6 +330,41 @@ def thunderstore(c: Context, skip_dependencies: bool = False, name: str = None, 
         
     for package in package_list:
         package.resolve(c, skip_dependencies)
+
+
+@task (
+    help={
+        'apworld_path': "Direct filepath for the apworld's directory, by default this scans for any folder within the '/apworld' directory",
+        'output_path': "Path to write the resulting apworld to. Defaults to the 'build' directory.",
+        'name': "Name for the output .apworld file, defaults to original directory name",
+        'zip': "Output as '.zip' rather than '.apworld'"
+    }
+)
+def apworld(c: Context, apworld_path: str = None, output_path: str = None, name: str = None, zip: bool = False):
+    """
+    Packages APWorlds into '.apworld's and adds 'archipelago.json' manifest files following Archipelago's "Build APWorlds" component.
+
+    For more information see https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/apworld%20specification.md#build-apworlds-launcher-component
+    """
+
+    if apworld_path:
+        apworlds = [Path(apworld_path)]
+    else:
+        apworlds = [directory for directory in p.root_dir.joinpath("apworld").glob("*") if directory.is_dir()]
+
+    if output_path:
+        apworlds_folder = Path(output_path)
+    else:
+        apworlds_folder = p.build_dir.joinpath("apworlds")
+    apworlds_folder.mkdir(parents=True, exist_ok=True)
+
+    # TODO: handle manifests like Archipelago's "Build APWorlds" component
+    print_task_header("Packaging APWorld(s)..")
+    for apworld in apworlds:
+        zip_path = os.path.join(apworlds_folder, apworld.name + ".apworld")
+        with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
+            for file in apworld.rglob("*"):
+                zf.write(file, Path(apworld.name, file.relative_to(apworld, walk_up=True)))
 
 
 @task (
